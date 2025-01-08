22334eb5cbec7ad8f9bd5ce889091079b5b714c8
Dmitry Karpeyev <dmitry.karpeyev@gmail.com>
2025-01-07 21:52:44 -0600

    gigapath/pancan/v1: docstr

--- seye/gigapath.py
+++ seye/gigapath.py
@@ -8,7 +8,35 @@
         Code is available at https://github.com/prov-gigapath/prov-gigapath, 
         pretrained model can be obtained from HuggingFace: https://huggingface.co/prov-gigapath/prov-gigapath.
 
-    
+    > rhubarb
+        * cluster info
+        ```
+            # basis capabilities:
+            scontrol show nodes
+            # current state
+            sinfo -N -l
+            # jobs
+            squeue
+        ```
+        * start an interactive job:
+        ```
+            # alloc
+            salloc --nodelist rhubarb -N 1 -n 1 --mem=64G
+            # job params:
+            env | grep SLURM_
+            env | grep SLURM_JOBID
+            # start interactive shell:
+            srun --jobid=$SLURM_JOBID --pty bash
+
+            # on rhubarb: BEGIN
+            nvidia-smi
+            htop
+            conda activate pancan-gigapath
+            # ...
+            exit
+            # on rhubarb: END
+
+            scancel $SLURM_JOBID
     > SETUP:
     ```
         export SEYE=$HOME/seye
