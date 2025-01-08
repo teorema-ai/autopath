e7b173d2795e7cfa7d994c8dc4dac56847c2d6d9
Dmitry Karpeyev <dmitry.karpeyev@gmail.com>
2025-01-07 21:36:19 -0600

    gigapath/pancan: debug

--- seye/gigapath.py
+++ seye/gigapath.py
@@ -365,7 +365,7 @@ class IMGS(Datablock):
                     print(f"IMGS: build: generating slides, slide_paths, slide_sources, source_slides: finished at {datetime.datetime.now()}")
         if self.verbose:
             print(f"TILES: Extracting tiles: started at {datetime.datetime.now()}")
-        set.dataset(scope, roots).extract_tiles(skip_extracted=not self.force_extract)
+        self.dataset(scope, roots).extract_tiles(skip_extracted=not self.force_extract)
         if self.verbose:
             print(f"TILES: Extracting tiles: finished at {datetime.datetime.now()}")
         
