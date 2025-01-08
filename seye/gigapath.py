91c303fd1397b7842cc7191faa3a8d99e1b0dd6e
Dmitry Karpeyev <dmitry.karpeyev@gmail.com>
2025-01-07 20:33:17 -0600

    gigapath/pancan: docstr

--- seye/gigapath.py
+++ seye/gigapath.py
@@ -46,7 +46,7 @@
                 ```
                     export CUDA_VISIBLE_DEVICES=0,1,2,3
                     export TCIMGS="DBX('seye.gigapath.IMGS', 'TCIMGS', repo='${SEYE}', revision='gigapath/pancan/${VERSION}').SCOPE(slides=DBX.Path('/mnt/labshare/SLIDES'), origin='TCGA', tile_px=256, tile_um=256)"
-                    dbx.print "$TCIMGS.Datablock(verbose=True).Databuilder(throw=True).build()"
+                    dbx.print "$TCIMGS.IMGS(verbose=True).Databuilder(throw=True).build()"
                     dbx.print "$TCIMGS.read('project')"
                     dbx.print "$TCIMGS.read('dataset')"
                     dbx.print "$TCIMGS.read('dataset').summary()"
