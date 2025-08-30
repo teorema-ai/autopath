d4ed9af74f3177b37c6d973272972f17ca2a21fb
Dmitry Karpeyev <dmitry.karpeyev@gmail.com>
2025-08-30 00:07:49 -0500

    gigaq: FeatureBatch example fix

--- autopath/pancan/features.py
+++ autopath/pancan/features.py
@@ -142,8 +142,7 @@ class TorchMultiprocessingDatabatchBuilder(DatabatchBuilder):
 		with MultiprocessProgress(pb) as mp_pb:
 			torch.multiprocessing.spawn(
 				datablock_method_multiprocessing,
-				args=(datablock_cls,
-					  datablock_method_args_kwargs_list,
+				args=(datablock_method_args_kwargs_list,
 					  mp_pb.tracker,
 					  slide_task,
 				),       
