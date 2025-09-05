7912f1684d714aa47e5c4a689a6b2d9300a52259
Dmitry Karpeyev <dmitry.karpeyev@gmail.com>
2025-09-04 23:33:51 -0500

    autopath: FeatureSet: DEBUG

--- autopath/gigaq/pipelines.py
+++ autopath/gigaq/pipelines.py
@@ -15,7 +15,7 @@ def GIGAPATH_BASELINE_CPTAC_8020_TEST_FEATURES(
     root=GIGAQ_DATASPACE,
     *,
     device = 'cuda',
-    gpu_batch_size=8,
+    gpu_batch_size=16,
     shard_size: int = 1024,
     verbose=False,
     debug=False,
