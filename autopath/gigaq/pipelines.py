0436d81a44d46bd210f62f3addbb2020dc22cd65
Dmitry Karpeyev <dmitry.karpeyev@gmail.com>
2025-09-22 21:39:23 -0500

    gigaq: TESTING gigapath_feature_bags() with sideband

--- autopath/gigaq/pipelines.py
+++ autopath/gigaq/pipelines.py
@@ -23,27 +23,7 @@ def gigapath_feature_bag(name, *, device = 'cuda', gpu_batch_size=1024,) -> Feat
         return FeatureBag(spec=dict(tilebag="@autopath.pancan.pipelines.pancan_tile_bag('CPTAC_SAMPLE')#",))
     else:
         raise ValueError(f"Unknown feature bag: {name}")
-
-
-# dbx "autopath.gigaq.pipelines.gigapath_feature_bags('GIGAPATH_BASELINE_CPTAC_8020_TEST').build()"
-def gigapath_feature_bags(name, *, devices = 'cuda', gpu_batch_size=1024) -> FeatureBags:
-    if name == "GIGAPATH_BASELINE_CPTAC_9802_TEST":
-        return FeatureBags(
-                    devices=devices,
-                    gpu_batch_size=gpu_batch_size,
-                    spec=dict(extractor="@autopath.gigaq.pipelines.gigapath_backbone_evaluator('GIGAPATH_BASELINE_BACKBONE_EVALUATOR')#",
-                              tilebags="@autopath.pancan.pipelines.pancan_tile_fold('CPTAC_9802_TEST')",
-        ))
-    elif name == "GIGAPATH_BASELINE_CPTAC_8020_TEST":
-        return FeatureBags(
-                    devices=devices,
-                    gpu_batch_size=gpu_batch_size,
-                    spec=dict(extractor="@autopath.gigaq.pipelines.gigapath_backbone_evaluator('GIGAPATH_BASELINE_BACKBONE_EVALUATOR')#",
-                              tilebags="@autopath.pancan.pipelines.pancan_tile_fold('CPTAC_8020_TEST')#",
-        ))
-    else:
-        raise ValueError(f"Unknown feature bags: {name}")
-
+        
 
 # dbx "autopath.gigaq.pipelines.gigapath_feature_bags('GIGAPATH_BASELINE_CPTAC_8020_TEST').build()"
 # dbx "autopath.gigaq.pipelines.gigapath_feature_bags('GIGAPATH_SIDEBAND_BASELINE_CPTAC_8020_TEST').build()"
