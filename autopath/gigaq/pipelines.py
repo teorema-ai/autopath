4bb8395c79391162f1b1905f44b37576a64bb077
Dmitry Karpeyev <dmitry.karpeyev@gmail.com>
2025-09-22 21:38:25 -0500

    gigaq: TESTING gigapath_feature_bags() with sideband

--- autopath/gigaq/pipelines.py
+++ autopath/gigaq/pipelines.py
@@ -32,7 +32,7 @@ def gigapath_feature_bags(name, *, devices = 'cuda', gpu_batch_size=1024) -> Fea
                     devices=devices,
                     gpu_batch_size=gpu_batch_size,
                     spec=dict(extractor="@autopath.gigaq.pipelines.gigapath_backbone_evaluator('GIGAPATH_BASELINE_BACKBONE_EVALUATOR')#",
-                              tilebags="@autopath.pancan.pipelines.pancan_tile_fold('CPTAC_9802_TEST')#",
+                              tilebags="@autopath.pancan.pipelines.pancan_tile_fold('CPTAC_9802_TEST')",
         ))
     elif name == "GIGAPATH_BASELINE_CPTAC_8020_TEST":
         return FeatureBags(
