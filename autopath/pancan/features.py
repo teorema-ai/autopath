ed781cc9e547ae7edbb9fc0547afcec4729f8d7c
Dmitry Karpeyev <dmitry.karpeyev@gmail.com>
2025-09-23 22:06:36 -0500

    gigaq: FeatureBags: sideband: TEST

--- autopath/pancan/features.py
+++ autopath/pancan/features.py
@@ -159,7 +159,7 @@ class FeatureBags(Datablock):
 				featurelen = self.__build_bag__(featurebag, extractor, device)
 			except Exception as e:
 				self.log.info(f"ERROR building feature bag {featurebag.hashpath()}: {e}")
-				tbstr = traceback.format_tb(e.__traceback__)
+				tbstr = '\n'.join(traceback.format_tb(e.__traceback__))
 				self.log.verbose(f"TRACEBACK: {tbstr}")
 				featurelen = 0
 			bag_lens.append(featurelen)
