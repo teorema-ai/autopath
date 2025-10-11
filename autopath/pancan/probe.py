507a5cc3435f4ac7564da6be1a71d68179072cb7
Dmitry Karpeyev <dmitry.karpeyev@gmail.com>
2025-10-10 23:12:03 -0500

    gigaq: FeaturesUniquePairwiseDistancesProbe: BUILD

--- autopath/pancan/probe.py
+++ autopath/pancan/probe.py
@@ -416,7 +416,9 @@ class FeaturesUniquePairwiseDistancesChunk(Datablock):
         #
         tensor = torch.zeros_like(_tensor_)
         for i in range(_tensor.shape[0]):
-            tensor[i, :] = _tensor_[i, original_order_indices]
+            row = _tensor_[i, original_order_indices]
+            self.log.debug(f"{i}: row.shape: {row.shape}")
+            tensor[i, :] = row
         return tensor
         
 
