39a4990e51959e31c4b9918676752c7beadcb11f
Dmitry Karpeyev <dmitry.karpeyev@gmail.com>
2025-10-10 23:04:33 -0500

    gigaq: FeaturesUniquePairwiseDistancesProbe: BUILD

--- autopath/pancan/probe.py
+++ autopath/pancan/probe.py
@@ -409,7 +409,9 @@ class FeaturesUniquePairwiseDistancesChunk(Datablock):
         original_order_indices = self.read('original_order_indices')
         _chunk = self.config.featuredist_chunk.read()
         _tensor = _chunk[row_subsample_indices, :][:, col_subsample_indices]
-        tensor = _tensor[:, original_order_indices]
+        tensor = torch.zeros_like(_tensor)
+        for i in range(_tensor.shape[0]):
+            tensor[i, :] = _tensor[i, original_order_indices]
         return tensor
         
 
