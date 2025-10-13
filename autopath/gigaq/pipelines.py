1be49ccea43e4166fd88d50033d163cb5cdbdba9
Dmitry Karpeyev <dmitry.karpeyev@gmail.com>
2025-10-12 22:37:32 -0500

    gigaq: Features2NNDimProbe: BUILD

--- autopath/gigaq/pipelines.py
+++ autopath/gigaq/pipelines.py
@@ -40,19 +40,19 @@ def gigapath_feature_bag(name, *, device = 'cuda', gpu_batch_size=1024,) -> Feat
 def gigapath_feature_bags(name, *, sideband: bool = False, capture_blocks: Optional[List[int]] = None, num_gpus: int = 1, gpu_batch_size=1024) -> FeatureBags:
     def get_extractor(name, sideband: bool = False, capture_blocks: Optional[List[int]] = None):
         if sideband: 
-            return f"@autopath.gigaq.pipelines.gigapath_backbone_evaluator('GIGAPATH_BASELINE_BACKBONE_EVALUATOR', sideband=True, capture_blocks={repr(capture_blocks)})"
+            return f"$autopath.gigaq.pipelines.gigapath_backbone_evaluator('GIGAPATH_BASELINE_BACKBONE_EVALUATOR', sideband=True, capture_blocks={repr(capture_blocks)})"
         else:
-            return f"@autopath.gigaq.pipelines.gigapath_backbone_evaluator('GIGAPATH_BASELINE_BACKBONE_EVALUATOR')"
+            return f"$autopath.gigaq.pipelines.gigapath_backbone_evaluator('GIGAPATH_BASELINE_BACKBONE_EVALUATOR')"
 
     if name == "GIGAPATH_BASELINE_SIDEBAND_CPTAC_9802_TEST":
         extractor=get_extractor('GIGAPATH_BASELINE_BACKBONE_EVALUATOR', sideband=sideband, capture_blocks=capture_blocks)
-        tilebags="@autopath.pancan.pipelines.pancan_tile_fold('CPTAC_9802_TEST')"
+        tilebags="$autopath.pancan.pipelines.pancan_tile_fold('CPTAC_9802_TEST')"
     elif name == "GIGAPATH_BASELINE_CPTAC_9802_TEST":
         extractor=get_extractor('GIGAPATH_BASELINE_BACKBONE_EVALUATOR', sideband=sideband, capture_blocks=capture_blocks)
-        tilebags="@autopath.pancan.pipelines.pancan_tile_fold('CPTAC_9802_TEST')"
+        tilebags="$autopath.pancan.pipelines.pancan_tile_fold('CPTAC_9802_TEST')"
     elif name == "GIGAPATH_BASELINE_CPTAC_8020_TEST":
         extractor=get_extractor('GIGAPATH_BASELINE_BACKBONE_EVALUATOR', sideband=sideband, capture_blocks=capture_blocks)
-        tilebags="@autopath.pancan.pipelines.pancan_tile_fold('CPTAC_8020_TEST')"
+        tilebags="$autopath.pancan.pipelines.pancan_tile_fold('CPTAC_8020_TEST')"
     else:
         raise ValueError(f"Unknown feature bags: {name}")
     devices = [f"cuda:{i}" for i in range(num_gpus)]
