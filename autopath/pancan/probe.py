8aa03e65aa846f5c9b14cd6e426c8f796e075d62
Dmitry Karpeyev <dmitry.karpeyev@gmail.com>
2025-10-13 22:52:04 -0500

    gigaq: Features2NNDim: BUILD

--- autopath/pancan/probe.py
+++ autopath/pancan/probe.py
@@ -552,15 +552,15 @@ class Features2NNDistancesChunk(Datablock):
         self.log.debug(f"Locating smallest nonzero pairwise distances in FeatureSortedDistancesChunk {sorted_distchunk.hashpath()} of shape {sorted_distchunk_tensor.shape}")
         firstdist = sorted_distchunk_tensor[:, 0]
         if self.debug:
-            firstdist_min, firstdist_max = firstdist.values.min(), firstdist.values.max()
+            firstdist_min, firstdist_max = firstdist.min(), firstdist.max()
             self.log.debug(f"firstdist_min: {firstdist_min}, firstdist_max: {firstdist_max}")
         self.log.debug(f"Locating second smallest pairwise distances in FeatureSortedDistancesChunk {self.config.featuredist_chunk.hashpath()} of shape {sorted_distchunk_tensor.shape}")
         seconddist = sorted_distchunk_tensor[:, 1]
         if self.debug:
-            seconddist_min, seconddist_max = seconddist.values.min(), seconddist.values.max()
+            seconddist_min, seconddist_max = seconddist.min(), seconddist.max()
             self.log.debug(f"seconddist_min: {seconddist_min}, seconddist_max: {seconddist_max}")
         del sorted_distchunk_tensor
-        twonn_distances = torch.stack([firstdist.values, seconddist.values], dim=-1)
+        twonn_distances = torch.stack([firstdist, seconddist], dim=-1)
         self.log.debug(f"Built Features2NNDistancesChunk {self.hashpath()} at offset {self.config.dist_chunk.config.row_batch_offset} with shape {twonn_distances.shape}")
         write_tensor(twonn_distances, self.path(ensure_dirpath=True))
         return self
