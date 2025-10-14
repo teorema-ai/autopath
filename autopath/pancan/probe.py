0c944109a3783b8b88b614932d4373b77369521e
Dmitry Karpeyev <dmitry.karpeyev@gmail.com>
2025-10-13 21:49:08 -0500

    gigaq: FeaturesPairwiseDistances: BUILD

--- autopath/pancan/probe.py
+++ autopath/pancan/probe.py
@@ -370,7 +370,7 @@ class FeaturesPairwiseDistances(Datablock):
         built_chunks = TorchMultithreadingDatashardBatchBuilder(devices=self.devices, log=self.log).build_shards(missing_chunks, features)
         self.log.verbose(f"Built all pairwise feature distance chunks: {len(built_chunks)}")
         write_tensor(torch.tensor([len(chunks)]), self.path('n_chunks', ensure_dirpath=True))
-        write_tensor(features.shape, self.path('features_shape', ensure_dirpath=True))
+        write_tensor(torch.tensor(features.shape), self.path('features_shape', ensure_dirpath=True))
         return self
     
     def __read__(self, topic):
