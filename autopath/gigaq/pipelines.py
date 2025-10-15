5b259fefd816a3a5545b955e2d45e4815c6ac24c
Dmitry Karpeyev <dmitry.karpeyev@gmail.com>
2025-10-15 10:12:04 -0500

    gigaq: Features2NNDim: BUILD

--- autopath/gigaq/pipelines.py
+++ autopath/gigaq/pipelines.py
@@ -112,6 +112,14 @@ def gigapath_features_pairwise_distances(name) -> FeaturesPairwiseDistances:
                               col_chunk_size=4000,
                     ), 
         )
+    elif name == "GIGAPATH_BASELINE_CPTAC_8020_TEST_5_2000_4000":
+        return FeaturesPairwiseDistances(
+                    spec=dict(featurebags=f"$autopath.gigaq.pipelines.gigapath_feature_bags('GIGAPATH_BASELINE_CPTAC_8020_TEST')",
+                              n_chunks=5,
+                              row_chunk_size=20000,
+                              col_chunk_size=4000,
+                    ), 
+        )
     else:
         raise ValueError(f"Unknown feature bags pairwise distances datablock: {name}")
 
