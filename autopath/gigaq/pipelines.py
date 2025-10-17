ec22f1b2fe8b9da33ad2a107479368c0b52001bf
Dmitry Karpeyev <dmitry.karpeyev@gmail.com>
2025-10-17 13:32:57 -0500

    gigaq: Features2NNDim: BUILD

--- autopath/gigaq/pipelines.py
+++ autopath/gigaq/pipelines.py
@@ -120,6 +120,38 @@ def gigapath_features_pairwise_distances(name) -> FeaturesPairwiseDistances:
                               col_chunk_size=4000,
                     ), 
         )
+    elif name == "GIGAPATH_BASELINE_CPTAC_8020_TEST_5_1000_5000":
+        return FeaturesPairwiseDistances(
+                    spec=dict(featurebags=f"$autopath.gigaq.pipelines.gigapath_feature_bags('GIGAPATH_BASELINE_CPTAC_8020_TEST')",
+                              n_chunks=5,
+                              row_chunk_size=1000,
+                              col_chunk_size=100000,
+                    ), 
+        )
+    elif name == "GIGAPATH_BASELINE_CPTAC_8020_TEST_5_1000_10000":
+        return FeaturesPairwiseDistances(
+                    spec=dict(featurebags=f"$autopath.gigaq.pipelines.gigapath_feature_bags('GIGAPATH_BASELINE_CPTAC_8020_TEST')",
+                              n_chunks=5,
+                              row_chunk_size=1000,
+                              col_chunk_size=100000,
+                    ), 
+        )
+    elif name == "GIGAPATH_BASELINE_CPTAC_8020_TEST_5_1000_20000":
+        return FeaturesPairwiseDistances(
+                    spec=dict(featurebags=f"$autopath.gigaq.pipelines.gigapath_feature_bags('GIGAPATH_BASELINE_CPTAC_8020_TEST')",
+                              n_chunks=5,
+                              row_chunk_size=1000,
+                              col_chunk_size=100000,
+                    ), 
+        )
+    elif name == "GIGAPATH_BASELINE_CPTAC_8020_TEST_5_1000_50000":
+        return FeaturesPairwiseDistances(
+                    spec=dict(featurebags=f"$autopath.gigaq.pipelines.gigapath_feature_bags('GIGAPATH_BASELINE_CPTAC_8020_TEST')",
+                              n_chunks=5,
+                              row_chunk_size=1000,
+                              col_chunk_size=100000,
+                    ), 
+        )
     elif name == "GIGAPATH_BASELINE_CPTAC_8020_TEST_5_1000_100000":
         return FeaturesPairwiseDistances(
                     spec=dict(featurebags=f"$autopath.gigaq.pipelines.gigapath_feature_bags('GIGAPATH_BASELINE_CPTAC_8020_TEST')",
