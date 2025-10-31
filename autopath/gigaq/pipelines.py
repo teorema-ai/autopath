8c59d692b9fc3046cbdeb406687d61579f7bebdd
Dmitry Karpeyev <dmitry.karpeyev@gmail.com>
2025-10-30 22:41:12 -0500

    gigaq: FeaturePairwiseDistances: BUILD

--- autopath/gigaq/pipelines.py
+++ autopath/gigaq/pipelines.py
@@ -268,12 +268,12 @@ def gigapath_features_2nn_dim_build_tree(name, n_workers: int = 1) -> Features2N
                     ).build()
 
 
-# git commit -am 'gigaq: FeaturePairwiseDistances: BUILD'; dbx.print 'autopath.gigaq.pipelines.gigapath_feature_pairwise_distances("GIGAPATH_BASELINE_5BLOCK0_CPTAC_8020_TEST_10_1000_800000").set(n_devices=3).build_tree()'
+# git commit -am 'gigaq: FeaturePairwiseDistances: BUILD'; dbx.print 'autopath.gigaq.pipelines.gigapath_feature_pairwise_distances("GIGAPATH_BASELINE_CPTAC_8020_TEST_10_1000_800000").set(n_devices=3).build_tree()'
 # git commit -am 'gigaq: FeaturePairwiseDistances: BUILD'; dbx.print 'autopath.gigaq.pipelines.gigapath_feature_pairwise_distances("GIGAPATH_BASELINE_5BLOCK0_CPTAC_9802_TEST_10_100_10000").set(n_devices=3).build_tree()'
 def gigapath_feature_pairwise_distances(name) -> FeaturePairwiseDistances:
-    if name == "GIGAPATH_BASELINE_5BLOCK0_CPTAC_8020_TEST_10_1000_800000":
+    if name == "GIGAPATH_BASELINE_CPTAC_8020_TEST_10_1000_800000":
         return FeaturePairwiseDistances(
-                    spec=dict(features=f"$autopath.gigaq.pipelines.gigapath_features('GIGAPATH_BASELINE_5BLOCKS_CPTAC_8020_TEST')",
+                    spec=dict(features=f"$autopath.gigaq.pipelines.gigapath_features('GIGAPATH_BASELINE_CPTAC_8020_TEST')",
                               layer="block.0",
                               n_shards=10,
                               row_shard_size=1000,
