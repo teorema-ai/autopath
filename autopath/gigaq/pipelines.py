efe3083b5df1d2c0e93bc2a9941ecc66d7758aeb
Dmitry Karpeyev <dmitry.karpeyev@gmail.com>
2025-10-30 22:03:46 -0500

    gigaq: FeaturePairwiseDistances: BUILD

--- autopath/gigaq/pipelines.py
+++ autopath/gigaq/pipelines.py
@@ -268,7 +268,7 @@ def gigapath_features_2nn_dim_build_tree(name, n_workers: int = 1) -> Features2N
                     ).build()
 
 
-# git commit -am "gigaq: FeaturePairwiseDistances: BUILD"; dbx.print "autopath.gigaq.pipelines.gigapath_feature_pairwise_distances('GIGAPATH_BASELINE_5BLOCK0_CPTAC_8020_TEST_10_1000_800000').set(n_devices=3).build()"
+# git commit -am 'gigaq: FeaturePairwiseDistances: BUILD'; dbx.print 'autopath.gigaq.pipelines.gigapath_feature_pairwise_distances("GIGAPATH_BASELINE_5BLOCK0_CPTAC_8020_TEST_10_1000_800000").set(n_devices=3).build_tree()'
 def gigapath_feature_pairwise_distances(name) -> FeaturePairwiseDistances:
     if name == "GIGAPATH_BASELINE_5BLOCK0_CPTAC_8020_TEST_10_1000_800000":
         return FeaturePairwiseDistances(
