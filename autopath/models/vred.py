30ce617b9fb96c05de1bcf599ad6eee1be373ca7
Dmitry Karpeyev <dmitry.karpeyev@gmail.com>
2025-11-20 20:55:38 -0600

    gigaq: Feature2NNDim: BUILD

--- autopath/models/vred.py
+++ autopath/models/vred.py
@@ -42,6 +42,10 @@ class Classifier(nn.Module):
             x = self.hidden_activations[i](x)
         x = self.last_layer(x)
         self.log.debug(f"Generated logits of shape: {x.shape=}")
+
+        #DEBUG
+        breakpoint()
+        
         x = F.softmax(x, dim=1)
         self.log.debug(f"Generated classes of shape: {x.shape=}")
         return x
