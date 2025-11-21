acb7d4671ccab2e9d605c422e1912c58dcd52fd0
Dmitry Karpeyev <dmitry.karpeyev@gmail.com>
2025-11-21 17:36:19 -0600

    gigaq: VRED eval: BUILD

--- autopath/models/vred.py
+++ autopath/models/vred.py
@@ -225,7 +225,7 @@ class VariationalReDecoder(nn.Module):
             variance_scale=variance_scale,
         )
         self.class_batch_size = class_batch_size
-        self.log = log or dbx.Logger(self.__class__)
+        self.log = log or dbx.Logger(self.__class__.__name__)
 
     @property
     def n_classes(self):
