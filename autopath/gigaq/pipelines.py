e886af76d33fcd11004331ec5aa28e775c35c099
Dmitry Karpeyev <dmitry.karpeyev@gmail.com>
2025-12-03 07:57:04 -0600

    gigaq: VRED: STILL: BUILD

--- autopath/gigaq/pipelines.py
+++ autopath/gigaq/pipelines.py
@@ -328,7 +328,7 @@ def gigapath_vred_evaluator(vred_dataset_name, **dataloader_kwargs):
 def gigapath_vred_lightning(vred_name, learning_rate: float = 0.03):
     return VariationalReDecoderLightning(spec=dict(vred=dbx.quote(gigapath_vred, vred_name), learning_rate=learning_rate))
 
-# git commit -am "gigaq: VRED: STILL: BUILD"; dbx.print "autopath.gigaq.pipelines.gigapath_vred_still('GIGAPATH_VRED_2HDN_100CLS_5CHN_BASELINE_CPTAC_8020_TEST', max_epochs=10, max_steps=10, batch_size=3, shuffle=True).to('cuda').build()"
+# git commit -am "gigaq: VRED: STILL: BUILD"; dbx.print "autopath.gigaq.pipelines.gigapath_vred_still('GIGAPATH_VRED_2HDN_100CLS_5CHN_BASELINE_CPTAC_8020_TEST', max_epochs=10, max_steps=10, batch_size=3, shuffle=False).to('cuda').build()"
 def gigapath_vred_still(vred_dataset_name, *, learning_rate: float = 0.03, max_epochs: int = 3, max_steps: int = 1, init_ckpt_path: str = None, n_devices: int = 1, batch_size: int = 1, shuffle: bool = False,):
     evaluator = gigapath_vred_evaluator(vred_dataset_name, batch_size=batch_size, shuffle=shuffle)
     lightning = VariationalReDecoderLightning(spec=dict(vred=evaluator.spec['vred'], learning_rate=learning_rate))
