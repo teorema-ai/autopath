3ad0b88dfe6d310d09692bb8e368d583c4454ee9
Dmitry Karpeyev <dmitry.karpeyev@gmail.com>
2025-12-03 21:43:55 -0600

    gigaq: VRED: STILL: BUILD

--- autopath/models/vred.py
+++ autopath/models/vred.py
@@ -496,8 +496,7 @@ class VariationalReDecoderStill(Datablock):
         )
         self.log.debug(f"Built {trainer=} for lightining {self.cfg.lightning}")
         self.log.debug(f"Launching training for {self.cfg.max_steps=}")
-        with torch.autograd.detect_anomaly(True):
-            trainer.fit(model=self.cfg.lightning.lightning_module, train_dataloaders=self.cfg.dataloader)
+        trainer.fit(model=self.cfg.lightning.lightning_module, train_dataloaders=self.cfg.dataloader)
         return self
 
   
