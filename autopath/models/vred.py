d3e97ca274aa16ae2f015210951b41efb693717a
Dmitry Karpeyev <dmitry.karpeyev@gmail.com>
2025-12-06 22:13:21 -0600

    gigaq: VRED: STILL: BUILD

--- autopath/models/vred.py
+++ autopath/models/vred.py
@@ -518,7 +518,7 @@ class VariationalReDecoderStill(Datablock):
         if self.logs is not None:
             self.log.verbose(f"---------------------- Linking logs to {self.logs}----------------------------")
             if os.path.exists(self.logs):
-                os.unlink(self.logs)
+                os.remove(self.logs)
             os.symlink(self.dirpath('logs', ensure=True), self.logs)
 
     def valid(self):
