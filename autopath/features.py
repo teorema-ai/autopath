a3fef551bd02bc1ef0f21d145a681d3d8da88851
Dmitry Karpeyev <dmitry.karpeyev@gmail.com>
2025-12-18 21:54:12 -0600

    gigaq: FeatureBagClip: BUILD

--- autopath/features.py
+++ autopath/features.py
@@ -179,8 +179,8 @@ class FeatureBagClip(Clip):
         self.log.detailed(f"Building bag_lens for bags with hashe paths {[bag.hashpath() for bag in bags]}")
         executor = MultithreadingCallableExecutor(n_threads=self.n_threads)
         executables = [FeatureBagClip.FeatureBagLengthComputer(bag) for bag in bags]
-        _bag_lens = executor.exec_callables(executables)
-        bag_lens = torch.tensor(itertools.chain.from_iterable(_bag_lens.values()))
+        bag_lens_lists = executor.exec_callables(executables)
+        bag_lens = torch.tensor(itertools.chain.from_iterable(bag_lens_lists))
         self.log.debug(f"bag_lens: {bag_lens}")
         self.log.verbose(f"Building bag_lens: END")
         dbx.write_tensor(bag_lens, self.path("bag_lens", ensure_dirpath=True))
