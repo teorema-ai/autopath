5e059c5134acb74e586adca770bceec8463484d3
Dmitry Karpeyev <dmitry.karpeyev@gmail.com>
2025-12-19 08:08:45 -0600

    gigaq: FeatureBagClip: BUILD

--- autopath/gigaq/pipelines.py
+++ autopath/gigaq/pipelines.py
@@ -84,6 +84,7 @@ def gigapath_feature_bag(name=None) -> FeatureBag:
         raise ValueError(f"Unknown feature shard: {name}")
 
 # git commit -am "gigaq: FeatureBagClip: BUILD"; dbx.print "autopath.gigaq.pipelines.gigapath_feature_bag_clip('GIGAPATH_BASELINE_CPTAC', n_devices=3, n_threads=16).set(gpu_batch_size=1024).build()"
+# git commit -am "gigaq: FeatureBagClip: BUILD"; dbx.print "autopath.gigaq.pipelines.gigapath_feature_bag_clip('GIGAPATH_BASELINE_CPTAC_9802_TEST', n_threads=16).build()"
 # git commit -am "gigaq: FeatureBagClip: BUILD"; dbx.print "autopath.gigaq.pipelines.gigapath_feature_bag_clip('GIGAPATH_BASELINE_CPTAC_8020_TEST', n_threads=16).build()"
 def gigapath_feature_bag_clip(name=None, n_devices: int = 1, n_threads: int = 1) -> FeatureBagClip:
     devices = [f'cuda:{i}' for i in range(n_devices)]
