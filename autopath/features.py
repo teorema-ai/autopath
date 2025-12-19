31ea5995f6dab558f1c73989b8a51ec70cde88f0
Dmitry Karpeyev <dmitry.karpeyev@gmail.com>
2025-12-18 20:27:59 -0600

    gigaq: FeatureSjard: BUILD

--- autopath/features.py
+++ autopath/features.py
@@ -313,7 +313,7 @@ class FeatureShard(Shard):
         lo, hi = offset, offset+self.cfg.shard_size
         indices = permutation[lo:hi]
         self.log.debug(f"Building a shard of size {self.cfg.shard_size} with index {self.cfg.index} from a dataset of len {dataset_len}")
-        self.log.detailed(f"Shard indices:{lo=}:{hi=}, {indices=}")
+        self.log.debug(f"Shard indices:{lo=}:{hi=}, {indices=}")
         _tensor_list, labels = zip(*[self.cfg.featureset[i] for i in indices])
         _labels_list, _tiles_list = zip(*labels)
         tensor = torch.stack(_tensor_list).numpy()
