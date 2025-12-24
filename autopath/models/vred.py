b8a66cbbbf90ffb9e77a52afc13595244a4bcc46
Dmitry Karpeyev <dmitry.karpeyev@gmail.com>
2025-12-24 10:01:51 -0600

    gigaq: VRED: STILL: BUILD

--- autopath/models/vred.py
+++ autopath/models/vred.py
@@ -228,7 +228,7 @@ class ConvDecoder2D(nn.Module):
         assert len(found_resolutions) == len(
             self.multiscale_resolutions
         ), f"Expected multiscale resolutions {self.multiscale_resolutions} but only found {found_resolutions}"
-        variance = self.variance_final_softplus(self.variance_final_fc(self.variance_finalconv(mean))) + torch.full((bs, mean.shape[1], height, width), self.variance_baseline).to(mean.device)
+        variance = self.variance_final_softplus(self.variance_final_fc(self.variance_final_conv(mean))) + torch.full((bs, mean.shape[1], height, width), self.variance_baseline).to(mean.device)
         mean = self.final_conv(mean)
         self.log.detailed(f"final_conv: mean: {mean.shape=}, {mean.device=}")
         self.log.detailed(f"final_conv: variance: {variance.shape=}, {variance.device=}")
