d0a9c229f06b8a8f7dc1ddfab7bf952209c3f443
Dmitry Karpeyev <dmitry.karpeyev@gmail.com>
2025-12-24 09:54:35 -0600

    gigaq: VRED: STILL: BUILD

--- autopath/models/vred.py
+++ autopath/models/vred.py
@@ -665,7 +665,7 @@ class VariationalReEncoderDecoderStill(Datablock):
                 self.log.info(f"Using checkpoint {ckpt}")
                 if not self.cfg.load_optimizer_state:
                     self.log.info(f"Skipping optimizer state from {ckpt}")
-                    checkpoint = torch.load(ckpt)
+                    checkpoint = torch.load(ckpt, weights_only=True)
                     model.load_state_dict(checkpoint['state_dict'])
                 else:
                     fit_kwargs['ckpt_path'] = ckpt
