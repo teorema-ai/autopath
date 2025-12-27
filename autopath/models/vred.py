b4c86d5561aac8205a772c6ee16ef37bf1c1c6d6
Dmitry Karpeyev <dmitry.karpeyev@gmail.com>
2025-12-26 20:20:58 -0600

    gigaq: VRED: STILL: BUILD

--- autopath/models/vred.py
+++ autopath/models/vred.py
@@ -662,11 +662,11 @@ class VariationalReEncoderDecoderLightning(Datablock):
                         self.log.detailed(f"Logging latent mixture distribution for class {k}, scale {s}: {latent_mean.shape=}, {latent_variance.shape=}")
                         #DEBUG
                         breakpoint()
-                        
+
                         for c in range(latent_mean.shape[0]):
-                            self.logger.experiment.add_image(f"latent_mix_mean/component={k}/scale={s}/channel={c}", latent_mean[c], self.global_step)
+                            self.logger.experiment.add_image(f"latent_mix_mean/component={k}/scale={s}/channel={c}", latent_mean[c:c+1], self.global_step)
                         for c in range(latent_variance.shape[0]):
-                            self.logger.experiment.add_scalar(f"latent_mix_variance/component={k}/scale={s}/channel={c}", latent_variance[c], self.global_step)
+                            self.logger.experiment.add_scalar(f"latent_mix_variance/component={k}/scale={s}/channel={c}", latent_variance[c:c+1], self.global_step)
             if self.log_feature_norms:
                 feature_norms = [torch.linalg.norm(features[i]) for i in range(len(features))]
                 self.logger.experiment.add_scalar(f"feature_norms_max", max(feature_norms), self.global_step)
