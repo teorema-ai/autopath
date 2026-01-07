50acdc098ae835deca0ccd812b256d9ab74d2527
Dmitry Karpeyev <dmitry.karpeyev@gmail.com>
2026-01-06 20:47:32 -0600

    gigaq: FeatureBagMedianProbe: BUILD

--- autopath/pancan/probes.py
+++ autopath/pancan/probes.py
@@ -250,7 +250,7 @@ class FeatureBagMedianProbe(Datablock):
             bag_feature_list.append(featurebag.features)
         self.log.verbose(f"READING featurebags: DONE")
         self.log.verbose(f"STACKING features and computing median: BEGIN")
-        features = torch.stack(bag_feature_list).numpy()
+        features = torch.cat(bag_feature_list, dim=0).numpy()
         median  = np.median(features, axis=0)
         write_npz(self.path('median', ensure_dirpath=True), median=median)
         self.log.verbose(f"STACKING features and computing median: BEGIN")
